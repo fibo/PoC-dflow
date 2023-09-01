04acbac52186f3b4d5d386d72c2f5c0bd009c929
fibo <fibo@users.noreply.github.com>
2023-09-01 22:40:23 +0200

    add web components

--- view.js
+++ view.js
@@ -1,55 +1,95 @@
-class Base extends HTMLElement {
-	constructor(template) {
-		super();
-		this.attachShadow({ mode: "open" });
-		this.shadowRoot?.appendChild(template.content.cloneNode(true));
-	}
-	static template(html) {
-		const template = document.createElement("template");
-		template.innerHTML = html;
-		return template;
-	}
-}
-const canvasTemplate = Base.template(`
-<style>
-  div {
-	border: 1px solid black;
-	width: 800px;
-	height: 600px;
+const html = (strings, ...expressions) => {
+  const template = document.createElement("template");
+  template.innerHTML = strings.reduce(
+    (result, string, index) => result + string + (expressions[index] ?? ""),
+    ""
+  );
+  return template;
+};
+class FlowView extends HTMLElement {
+  static Canvas = class FlowViewCanvas extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      this.shadowRoot?.appendChild(
+        FlowView.Canvas.template.content.cloneNode(true)
+      );
+    }
+    static tagName = "fv-canvas";
+    static template = html`
+			<style>
+				:host {
+					position: relative;
+					display: block;
+					overflow: hidden;
+					/* transform: translate(-10px, -10px); */
+				}
+				div {
+					border: 1px solid black;
+					width: 800px;
+					height: 600px;
+				}
+			</style>
+			<div><slot></slot></div>
+		`;
+  };
+  static Node = class FlowViewNode extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      this.shadowRoot?.appendChild(
+        FlowView.Node.template.content.cloneNode(true)
+      );
+      this.addEventListener("dragstart", this);
+    }
+    attributeChangedCallback(name, _oldValue, value) {
+      if (!isNaN(parseInt(value))) {
+        if (name === "x")
+          this.style.left = `${value}px`;
+        if (name === "y")
+          this.style.top = `${value}px`;
+      }
+    }
+    handleEvent(event) {
+      if (event instanceof DragEvent) {
+        console.log(event);
+      }
+    }
+    static get observedAttributes() {
+      return ["x", "y"];
+    }
+    static tagName = "fv-node";
+    static template = html`
+			<style>
+				:host {
+					position: absolute;
+				}
+				div {
+					border: 1px solid black;
+				}
+			</style>
+			<div>node</div>
+		`;
+    static Moved = class FlowViewNodeMoved extends CustomEvent {
+      constructor(detail) {
+        super(FlowView.Node.Moved.eventName, { detail });
+      }
+      static eventName = "fv-nodemoved";
+    };
+    static Selected = class FlowViewNodeSelected extends CustomEvent {
+      constructor(detail) {
+        super(FlowView.Node.Selected.eventName, { detail });
+      }
+      static eventName = "fv-nodeselected";
+    };
+  };
+  static customElements = (/* @__PURE__ */ new Map()).set(FlowView.Canvas.tagName, FlowView.Canvas).set(FlowView.Node.tagName, FlowView.Node);
+  static defineCustomElements() {
+    for (let [tagName, CustomElement] of FlowView.customElements.entries())
+      if (!customElements.get(tagName))
+        customElements.define(tagName, CustomElement);
   }
-</style>
-<div><slot></slot></div>
-`);
-class Canvas extends Base {
-	constructor() {
-		super(canvasTemplate);
-		this.addEventListener("dragover", this);
-		this.addEventListener("drop", this);
-	}
-	handleEvent(event) {
-		if (event.type === "dragover") event.preventDefault();
-		if (event.type === "drop") console.log(event);
-	}
 }
-const nodeTemplate = Base.template(`
-<style>
-  div {
-	border: 1px solid black;
-	width: 100px;
-	height: 20px;
-  }
-</style>
-<div>node</div>
-`);
-class Node extends Base {
-	constructor() {
-		super(nodeTemplate);
-		this.addEventListener("dragstart", this);
-	}
-	handleEvent(event) {
-		if (event instanceof DragEvent) {
-			console.log(event);
-		}
-	}
-}
-export { Canvas, Node };
+export {
+  FlowView
+};
--- view.ts
+++ view.ts
@@ -1,3 +1,10 @@
+export declare namespace FlowView {
+	type NodeId = string;
+	type Position = { x: number; y: number };
+	export type NodeMovedEventDetail = { nodeId: FlowView.NodeId } & Position;
+	export type NodeSelectedEventDetail = { nodeId: FlowView.NodeId };
+}
+
 const html = (strings: TemplateStringsArray, ...expressions: string[]) => {
 	const template = document.createElement("template");
 	template.innerHTML = strings.reduce(
@@ -7,53 +14,101 @@ const html = (strings: TemplateStringsArray, ...expressions: string[]) => {
 	return template;
 };
 
-export class Canvas extends HTMLElement {
-	constructor() {
-		super();
-		this.attachShadow({ mode: "open" });
-		this.shadowRoot?.appendChild(Canvas.template.content.cloneNode(true));
-	}
-	static template = html`
-		<style>
-			:host {
-				position: relative;
-				display: block;
-				overflow: hidden;
-			}
-			div {
-				border: 1px solid black;
-				width: 800px;
-				height: 600px;
-			}
-		</style>
-		<div><slot></slot></div>
-	`;
-}
+export class FlowView extends HTMLElement {
+	static Canvas = class FlowViewCanvas extends HTMLElement {
+		constructor() {
+			super();
+			this.attachShadow({ mode: "open" });
+			this.shadowRoot?.appendChild(
+				FlowView.Canvas.template.content.cloneNode(true),
+			);
+		}
+		static tagName = "fv-canvas";
+		static template = html`
+			<style>
+				:host {
+					position: relative;
+					display: block;
+					overflow: hidden;
+					/* transform: translate(-10px, -10px); */
+				}
+				div {
+					border: 1px solid black;
+					width: 800px;
+					height: 600px;
+				}
+			</style>
+			<div><slot></slot></div>
+		`;
+	};
 
-export class Node extends HTMLElement {
-	constructor() {
-		super();
-		this.attachShadow({ mode: "open" });
-		this.shadowRoot?.appendChild(Node.template.content.cloneNode(true));
-		this.addEventListener("dragstart", this);
-	}
-	handleEvent(event: Event) {
-		if (event instanceof DragEvent) {
-			console.log(event);
+	static Node = class FlowViewNode extends HTMLElement {
+		constructor() {
+			super();
+			this.attachShadow({ mode: "open" });
+			this.shadowRoot?.appendChild(
+				FlowView.Node.template.content.cloneNode(true),
+			);
+			this.addEventListener("dragstart", this);
 		}
-	}
-static template = html`
-	<style>
-		:host {
-			position: absolute;
+		attributeChangedCallback(
+			name: string,
+			_oldValue: string,
+			value: string,
+		) {
+			if (!isNaN(parseInt(value))) {
+				if (name === "x") this.style.left = `${value}px`;
+				if (name === "y") this.style.top = `${value}px`;
+			}
 		}
-		div {
-			border: 1px solid black;
-			width: 100px;
-			height: 20px;
+		handleEvent(event: Event) {
+			if (event instanceof DragEvent) {
+				console.log(event);
+			}
 		}
-	</style>
-	<div>node</div>
-`;
+		static get observedAttributes() {
+			return ["x", "y"];
+		}
+		static tagName = "fv-node";
+		static template = html`
+			<style>
+				:host {
+					position: absolute;
+				}
+				div {
+					border: 1px solid black;
+				}
+			</style>
+			<div>node</div>
+		`;
+		static Moved = class FlowViewNodeMoved extends CustomEvent<{
+			nodeId: string;
+		}> {
+			constructor(detail: { nodeId: string }) {
+				super(FlowView.Node.Moved.eventName, { detail });
+			}
+			static eventName = "fv-nodemoved";
+		};
+		static Selected = class FlowViewNodeSelected extends CustomEvent<{
+			nodeId: string;
+		}> {
+			constructor(detail: { nodeId: string }) {
+				super(FlowView.Node.Selected.eventName, { detail });
+			}
+			static eventName = "fv-nodeselected";
+		};
+	};
 
+	static customElements = new Map<
+		string,
+		typeof FlowView.Canvas | typeof FlowView.Node
+	>()
+		.set(FlowView.Canvas.tagName, FlowView.Canvas)
+		.set(FlowView.Node.tagName, FlowView.Node);
+
+	static defineCustomElements() {
+		for (let [tagName, CustomElement] of FlowView.customElements.entries())
+			if (!customElements.get(tagName))
+				customElements.define(tagName, CustomElement);
+	}
 }
